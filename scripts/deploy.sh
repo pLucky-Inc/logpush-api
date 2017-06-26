c09a288cd727893dbb183551d70d54451e2aa02b
tiqwab (Naohisa Murakami) <nao-murakami@i-mobile.co.jp>
2017-06-26 16:37:15 +0900

    git push with ssh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -24,8 +24,10 @@ git config user.email "kawachi@p-lucky.net"
 git add .
 git commit -m "Deploy to GitHub Pages"
 
+# Add remote repository
+git remote add origin "git@github.com:pLucky-Inc/logpush-api-docs.git"
+
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
-# will be lost, since we are overwriting it.) We redirect any output to
-# /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+# will be lost, since we are overwriting it.)
+git push --force --quiet origin master:gh-pages
