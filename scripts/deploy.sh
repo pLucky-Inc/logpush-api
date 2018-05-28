ee9b53b9b58aae52fd02aef19776b983f8fe72a4
tiqwab (Naohisa Murakami) <nao-murakami@i-mobile.co.jp>
2018-05-28 12:24:15 +0900

    Set up for CircleCI 2.0

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,7 +17,7 @@ git init
 
 # inside this git repo we'll pretend to be a new user
 git config user.name "Circle CI"
-git config user.email "hayashi@p-lucky.net"
+git config user.email "admin-ghdev@i-mobile.co.jp"
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
